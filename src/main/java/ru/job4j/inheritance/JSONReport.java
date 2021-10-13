a442ce9bc0f2c0fd5be1644d9c45e9ac6bd939a5
maximova-ekaterina <skoropeda@yandex.ru>
2021-10-13 12:48:32 +0300

    4. Переопределение [#174890 #215164]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,6 +4,6 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return name + " :" + " name" + System.lineSeparator() + body + " :" + " body";
+        return "{\n" + "\"" + name + "\"" + " : " + "\"" + "name" + "\"" + ",\n" + "\"" + body + "\"" + " : " + "\"" + "body" + "\"\n" + "}\n";
     }
 }
