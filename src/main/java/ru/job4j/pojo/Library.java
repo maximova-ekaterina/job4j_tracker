25a5b42194c5c026055637b88d33c766e167cc3c
maximova-ekaterina <skoropeda@yandex.ru>
2021-10-18 15:24:53 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book fairyTail = new Book("The little prince", 15);
+        Book novel = new Book("War and peace", 1200);
+        Book biography = new Book("Jack-If-Custo", 355);
+        Book blank = new Book("Clean code", 0);
+        Book[] books = new Book[4];
+        books[0] = fairyTail;
+        books[1] = novel;
+        books[2] = blank;
+        books[3] = biography;;
+        for (int i = 0; i < books.length; i++) {
+            Book cC = books[i];
+            System.out.println(cC.getName() + " " + cC.getPageCount());
+        }
+        System.out.println("Replaced fairyTail and biography");
+        books[0] = biography;
+        books[3] = fairyTail;
+        for (int i = 0; i < books.length; i++) {
+            Book cC = books[i];
+            System.out.println(cC.getName() + "-" + cC.getPageCount());
+        }
+        System.out.println("Show empty book");
+        for (int i = 0; i < books.length; i++) {
+            Book cC = books[i];
+            if(cC.getName() == "Clean code") {
+                System.out.println(cC.getName() + "-" + cC.getPageCount());
+            }
+        }
+    }
+}
