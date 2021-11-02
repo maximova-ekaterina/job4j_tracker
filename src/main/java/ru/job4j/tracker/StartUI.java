7d667a44cb2c42358796f9ac1392b75b8bc16050
maximova-ekaterina <skoropeda@yandex.ru>
2021-11-02 13:17:13 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,16 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
 
     public static void main(String[] args) {
-        Item item = new Item();
-        System.out.println("Дата и время до форматирования: " + item.getCreated());
-
-        DateTimeFormatter restyle = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String sample = item.getCreated().format(restyle);
-        System.out.println("Дата и время после форматирования: " + sample);
+        Item item = new Item(2, "Asus");
+        System.out.println(item);
     }
 }
