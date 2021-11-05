e4d7db11a09eddf58f0db875e7ad9d56fc51c8fe
maximova-ekaterina <skoropeda@yandex.ru>
2021-11-05 21:33:08 +0300

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -40,13 +40,23 @@ public class StartUI {
                     System.out.println("Ошибка замены заявки.");
                 }
             } else if (select == 3) {
-                System.out.println("===Delete item");
+                System.out.println("===Delete item===");
                 System.out.println("Enter id: ");
                 int id = Integer.parseInt(scanner.nextLine());
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена успешно.");
                 } else {
                     System.out.println("Ошибка удаления заявки.");
+                }
+            } else if (select == 4) {
+                System.out.println("===Find by id===");
+                System.out.println("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Заявка с веденным id: " + id + " не найдена.");
                 }
             } else if (select == 6) {
                 run = false;
