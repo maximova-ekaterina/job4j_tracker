bca500ebc1344f865f1313db72361cd037cb1cd9
maximova-ekaterina <skoropeda@yandex.ru>
2021-11-21 15:57:55 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780 #230391]

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -12,6 +12,7 @@ public class CreateAction implements UserAction {
         String name = input.askStr("Enter name");
         Item item = new Item(name);
         tracker.add(item);
+        System.out.println("Added Item: " + item);
         return true;
     }
 }
