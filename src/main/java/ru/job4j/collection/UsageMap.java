ad182f30ca1202429823e4130fb22a87db3b7500
maximova-ekaterina <skoropeda@yandex.ru>
2022-03-14 16:20:13 +0300

    0. Map, HashMap [#212652 #274302]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,13 +1,15 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> post = new HashMap<>();
         post.put("skoropeda@yandex.ru", "Maksimova Ekaterina");
         post.put("alinka-kotova@mail.ru", "Kotova Alina");
-        for (String key : post.keySet()) {
+        post.put("alinka-kotova@mail.ru", "Kotova Polina");
+        for (Map.Entry<String, String> key : post.entrySet()) {
             System.out.println(key);
         }
     }
